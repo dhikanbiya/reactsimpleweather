0d09511a8e15ad6d6b572073599f2f4fae68dbcd
danbiya <dhika.rizki@bppt.go.id>
2020-07-12 09:26:03 +0700

    tyding up interface

--- src/pages/city.js
+++ src/pages/city.js
@@ -1,5 +1,6 @@
-import React, {useState,useEfect, useEffect} from 'react';
+import React, {useState, useEffect} from 'react';
 import apiKey from '../key/apikey.js'
+import {Button,Form,Container,Col,Row} from 'react-bootstrap'
 var axios = require('axios')
 var moment = require('moment')
 
@@ -13,29 +14,29 @@ function City(){
     ]
     const [city, setCity] = useState('Jakarta');
     const [weather, setWeather] = useState({ daily:[] });
-    
-    useEffect(async () => {
-
-        const result = await axios(
-            `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&APPID=${apiKey}`,
-        );        
-        const dailyData = result.data.list.filter(only => only.dt_txt.includes("18:00:00"))
-        setWeather({daily:dailyData})    
+    const weatherAPI = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&APPID=${apiKey}`
+    useEffect(() => {
+        const fetchD = async ()=>{
+            const result = await axios(
+                weatherAPI,
+            );        
+            const dailyData = result.data.list.filter(only => only.dt_txt.includes("18:00:00"))
+            setWeather({daily:dailyData})          
+        }
         
+        fetchD();
       
     },[]);
    
     const getForecast = async (event)=>{
         event.preventDefault()
         console.log(event.target.value)
-        setCity(event.target.value)
-        const handleuri= `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&APPID=${apiKey}`
+        setCity(event.target.value)        
         const result = await axios(
-            handleuri,
+            weatherAPI,
         );        
         const dailyData = result.data.list.filter(only => only.dt_txt.includes("18:00:00"))
-        setWeather({daily:dailyData})    
-        console.log("handle " + handleuri)
+        setWeather({daily:dailyData})            
     }   
   
     
@@ -44,27 +45,29 @@ function City(){
    
    return (
     <div>
-    <form onSubmit={getForecast}>
-     Select your city   <select
-        onChange={(e)=> setCity(e.target.value)}
-     >
-      {cities.map(c => (
+    <h4>Get 5 days of Weather Forecast</h4>
+    <form onSubmit={getForecast}>     
+    <Form.Group controlId="selectCity">
+    <Form.Label>Select City</Form.Label>
+    <Form.Control as="select" onChange={(e)=> setCity(e.target.value)}>
+    {cities.map(c => (
         <option
           key={c.label}
           value={c.value}
         >
-          {c.value}
+           {c.value} 
         </option>
       ))}
-    </select>
-    <button type="submit">get forecast</button>
-    </form>
-      <ul>        
-        {weather.daily.map((item,index)=>(
-            <li key={index}>{moment(item.dt_txt).format('MMMM Do, h:mm a')} - {item.main.temp} Celcius - {item.weather[0].description}</li>
-        ))}
-      </ul>     
- 
+    </Form.Control>
+  </Form.Group>
+    <Button variant="primary" type="submit">Get Forecast</Button>{' '}
+    </form>    
+    <ul>        
+                {weather.daily.map((item,index)=>(
+                    <li key={index}>{moment(item.dt_txt).format('MMMM Do, h:mm a')} - {item.main.temp} Celcius - {item.weather[0].description}</li>
+                ))}
+            </ul>  
+      
     </div>
    )
  
